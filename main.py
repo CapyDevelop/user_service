8d562ea10c17b7b5bf3f7d68edc9e0f518bdf569
Alexsei Popov <popov2895@gmail.com>
2023-11-08 15:50:06 +0300

    add actions and styled

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from concurrent import futures
+
+import grpc
+from dotenv import load_dotenv
+
+import user_service.user_service_pb2_grpc as user_pb2_grpc
+from user_info_service import UssrSservice
+
+load_dotenv()
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    user_pb2_grpc.add_UserServiceServicer_to_server(UssrSservice(), server)
+    server.add_insecure_port(f'[::]:{os.getenv("USER_SERVICE_PORT")}')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    serve()
---
+++ user_info_service/__init__.py
@@ -0,0 +1,58 @@
+import logging
+import os
+
+import auth_service.authservice_pb2 as auth_pb2
+import auth_service.authservice_pb2_grpc as auth_pb2_grpc
+import grpc
+import school_service.school_service_pb2 as school_pb2
+import school_service.school_service_pb2_grpc as school_pb2_grpc
+from dotenv import load_dotenv
+
+import user_service.user_service_pb2 as user_pb2
+import user_service.user_service_pb2_grpc as user_pb2_grpc
+
+load_dotenv()
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - '
+                           '%(levelname)s - %(message)s')
+
+auth_service_channel = grpc.insecure_channel(
+    f"{os.getenv('AUTH_SERVICE_HOST')}:{os.getenv('AUTH_SERVICE_PORT')}"
+)
+auth_service_stub = auth_pb2_grpc.AuthServiceStub(auth_service_channel)
+
+school_service_channel = grpc.insecure_channel(
+    f"{os.getenv('SCHOOL_SERVICE_HOST')}:{os.getenv('SCHOOL_SERVICE_PORT')}"
+)
+school_service_stub = school_pb2_grpc.SchoolServiceStub(school_service_channel)
+
+
+class UssrSservice(user_pb2_grpc.UserServiceServicer):
+    def get_rp(self, request, context):
+        logging.info("[ Get rp ] - Get rp request. ----- START -----")
+        logging.info("[ Get rp ] - request to auth_service")
+        auth_info_request = auth_pb2.TokenRequest(uuid=request.capy_uuid)
+        auth_info_response = auth_service_stub.get_token_by_uuid(auth_info_request)
+        logging.info("[ Get rp ] - response from auth_service")
+        if auth_info_response.status != 0:
+            logging.info("[ Get rp ] - Error response from auth_service. ----- END -----")
+            return user_pb2.GetRpResponse(
+                status=1
+            )
+        logging.info("[ Get rp ] - request to school_service")
+        school_info_request = school_pb2.GetRpRequest(
+            access_token=auth_info_response.access_token
+        )
+        school_info_response = school_service_stub.get_rp_info(school_info_request)
+        logging.info("[ Get rp ] - response from school_service")
+        return user_pb2.GetRpResponse(
+            coins=school_info_response.coins,
+            prp=school_info_response.prp,
+            crp=school_info_response.crp,
+            level=school_info_response.level,
+            first_name=school_info_response.first_name,
+            last_name=school_info_response.last_name,
+            login=school_info_response.login,
+            status=0,
+            description="Success"
+        )
